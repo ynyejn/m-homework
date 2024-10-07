5e654d25aa1596edec350ded27a105bc79b5f8e0
ynyejn <yeounshi@gmail.com>
2024-10-08 01:08:56 +0900

    에러 테스트케이스 추가

--- src/test/groovy/com/musinsa/homework/controller/api/AdminApiControllerItTest.groovy
+++ src/test/groovy/com/musinsa/homework/controller/api/AdminApiControllerItTest.groovy
@@ -1,6 +1,7 @@
 package com.musinsa.homework.controller.api
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.musinsa.homework.entity.Brand
 import com.musinsa.homework.repository.BrandRepository
 import com.musinsa.homework.repository.item.ItemRepository
 import com.musinsa.homework.service.AdminApplicationService
@@ -125,5 +126,100 @@ class AdminApiControllerItTest extends Specification {
         and:
         def deletedItem = itemRepository.findById(createdItem.id)
         deletedItem.isEmpty()
+    }
+
+
+    def "브랜드 생성 시 이미 존재하는 브랜드명이면 에러를 반환한다."() {
+        given:
+        def brandName = "브랜드A"
+        brandRepository.save(new Brand(brandName))
+
+        when:
+        def result = mockMvc.perform(
+                MockMvcRequestBuilders.post("/v1/admin/brand-item-management")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "brandOperation": {
+                                    "action": "CREATE",
+                                    "brandName": "$brandName"
+                                }
+                            }
+                        """)
+        )
+
+        then:
+        result.andExpect(status().isBadRequest())
+                .andExpect(jsonPath('$.success').value(false))
+                .andExpect(jsonPath('$.message').value("이미 존재하는 브랜드 명입니다."))
+    }
+
+    def "BrandOperation과 ItemOperation이 동시에 입력되면 에러가 발생한다"() {
+        when:
+        def result = mockMvc.perform(
+                MockMvcRequestBuilders.post("/v1/admin/brand-item-management")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "brandOperation": {
+                                    "action": "CREATE",
+                                    "brandName": "브랜드A"
+                                },
+                                "itemOperation": {
+                                    "action": "CREATE",
+                                    "brandId": 1,
+                                    "categoryId": 1,
+                                    "price": 10000
+                                }
+                            }
+                        """)
+        )
+
+        then:
+        result.andExpect(status().isBadRequest())
+                .andExpect(jsonPath('$.success').value(false))
+                .andExpect(jsonPath('$.message').value("BrandOperation 또는 ItemOperation 중 하나만 입력되어야 합니다."))
+    }
+    def "상품 생성 시 가격이 음수이면 에러가 발생한다"() {
+        when:
+        def result = mockMvc.perform(
+                MockMvcRequestBuilders.post("/v1/admin/brand-item-management")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "itemOperation": {
+                                    "action": "CREATE",
+                                    "brandId": 1,
+                                    "categoryId": 1,
+                                    "price": -1000
+                                }
+                            }
+                        """)
+        )
+
+        then:
+        result.andExpect(status().isBadRequest())
+                .andExpect(jsonPath('$.success').value(false))
+                .andExpect(jsonPath('$.message').value("상품 가격은 0 이상이어야 합니다."))
+    }
+
+    def "필수 필드가 누락되면 에러가 발생한다"() {
+        when:
+        def result = mockMvc.perform(
+                MockMvcRequestBuilders.post("/v1/admin/brand-item-management")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "brandOperation": {
+                                    "action": "CREATE"
+                                }
+                            }
+                        """)
+        )
+
+        then:
+        result.andExpect(status().isBadRequest())
+                .andExpect(jsonPath('$.success').value(false))
+                .andExpect(jsonPath('$.message').value("브랜드 이름은 필수입니다."))
     }
 }
